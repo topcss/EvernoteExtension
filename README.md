d3965bf26b35768a092bf7e3b6f2e7bda7e35ddc
Jack <topcss@gmail.com>
2019-11-18 14:10:17 +0800

    Add some features.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EvernoteExtension
+
 网页版印象笔记功能的扩展（若干新功能，有我用起来才顺手）
+
+❤印象笔记插件
+
+``` JS
+javascript:void (function(d) {var e = d.createElement('script');e.byebj=true;e.src = 'https://topcss.github.io/EvernoteExtension/main.js';var b = d.getElementsByTagName('body')[0];b.firstChild ? b.insertBefore(e, b.firstChild) : b.appendChild(e);}(document));
+```
--- main.js
+++ main.js
@@ -1,1 +1,219 @@
-alert('hello world!');
+
+function loadStyles (url) {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = url;
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(link);
+}
+
+function createEl (className, name = 'div') {
+  let el = document.createElement(name)
+  el.className = className
+  return el
+}
+
+function createButton (src, className) {
+  let img = createEl('', 'img')
+  img.src = "icons/" + src + ".svg"
+
+  let tbic = createEl('toolbar-button-icon-container')
+  let db = createEl('dui-badge', 'span')
+  let tbw = createEl('toolbar-button-wrapper ' + className)
+
+  tbic.appendChild(img)
+  db.appendChild(tbic)
+  tbw.appendChild(db)
+
+  return tbw
+}
+
+
+class BaseButton {
+  constructor() {
+  }
+}
+
+class UndoButton extends BaseButton {
+  constructor() {
+    super()
+
+    this.render()
+  }
+  render () {
+    this.el = createButton('undo', 'disabled')
+
+    this.el.addEventListener('click', function () {
+      tinyMCE.activeEditor.execCommand('undo')
+    })
+  }
+  update () {
+    var el = this.el
+
+    if (tinyMCE.activeEditor.undoManager.hasUndo()) {
+      el.className = el.className.replace(' disabled', '');
+    } else {
+      if (el.className.indexOf('disabled') === -1) {
+        el.className += ' disabled';
+      }
+    }
+  }
+}
+
+class RedoButton extends BaseButton {
+  constructor() {
+    super()
+
+    this.render()
+  }
+  render () {
+    this.el = createButton('redo', 'disabled')
+    this.el.addEventListener('click', function () {
+      tinyMCE.activeEditor.execCommand('redo')
+    })
+  }
+  update () {
+    var el = this.el
+
+    if (tinyMCE.activeEditor.undoManager.hasRedo()) {
+      el.className = el.className.replace(' disabled', '');
+    } else {
+      if (el.className.indexOf('disabled') === -1) {
+        el.className += ' disabled';
+      }
+    }
+  }
+}
+
+class RfButton extends BaseButton {
+  constructor() {
+    super()
+
+    this.render()
+  }
+  render () {
+    this.el = createButton('clear')
+    this.el.addEventListener('click', function () {
+      tinyMCE.activeEditor.execCommand('removeformat')
+    })
+  }
+}
+
+class HightlightButton extends BaseButton {
+  constructor() {
+    super()
+
+    this.render()
+  }
+  render () {
+    this.el = createButton('highlight')
+    this.el.addEventListener('click', function () {
+      tinyMCE.activeEditor.execCommand('hilitecolor', false, 'yellow')
+    })
+  }
+}
+
+class dividerButton extends BaseButton {
+  constructor() {
+    super()
+
+    this.render()
+  }
+  render () {
+    this.el = createEl('toolbar-button-divider')
+  }
+}
+
+class Toolbar {
+  constructor() {
+    this.render()
+  }
+  render () {
+    this.buttons = createEl('toolbar-buttons')
+
+    let tc = createEl('toolbar-container')
+    tc.appendChild(this.buttons)
+
+    this.el = createEl('toolbar-wrapper')
+    this.el.appendChild(tc)
+  }
+  addButton (btn) {
+    this.buttons.appendChild(btn.el)
+  }
+}
+
+class Subject {
+  constructor() {
+    this.subs = [];
+  }
+  addSub (sub) {
+    this.subs.push(sub);
+  }
+  notify () {
+    this.subs.forEach(sub => { sub.update(); });
+  }
+}
+
+function addToolbar () {
+
+  // 观察者
+  const subject = new Subject();
+
+  // 工具条
+  var toolbar = new Toolbar()
+
+  // 重做
+  var undoBtn = new UndoButton()
+  toolbar.addButton(undoBtn)
+
+  // 撤销
+  var redoBtn = new RedoButton()
+  toolbar.addButton(redoBtn)
+
+  // 分隔符
+  var dividerBtn = new dividerButton()
+  toolbar.addButton(dividerBtn)
+
+  // 清除格式
+  var rfBtn = new RfButton()
+  toolbar.addButton(rfBtn)
+
+  // 高亮
+  var highlightBtn = new HightlightButton()
+  toolbar.addButton(highlightBtn)
+
+  // 加入观察
+  subject.addSub(undoBtn)
+  subject.addSub(redoBtn)
+
+  // 载入css
+  loadStyles('style.css')
+
+  // 追加到 body
+  document.getElementsByTagName('body')[0].appendChild(toolbar.el)
+
+  // tb.appendChild(createButton('redo'))
+  // tb.appendChild(createButton('undo', 'disabled'))
+  // toolbar.appendChild(createEl('toolbar-button-divider'))
+  // toolbar.appendChild(createButton('brush', 'checked'))
+  // toolbar.appendChild(createButton('fold'))
+  // toolbar.appendChild(createButton('unfold'))
+  // toolbar.appendChild(createEl('toolbar-button-divider'))
+  // toolbar.appendChild(createButton('highlight'))
+
+  tinymce.activeEditor.on("NodeChange", function (e) {
+    subject.notify()
+  });
+}
+
+// var editor = tinyMCE.activeEditor;
+
+// tinyMCE.activeEditor.undoManager.hasRedo()
+
+
+setTimeout(function () {
+
+  addToolbar();
+
+}, 1000)
